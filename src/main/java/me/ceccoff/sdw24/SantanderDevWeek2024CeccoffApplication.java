3d8ee61bf117a31a9af75e51848308f7466b4a2a
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-03-30 17:23:37 -0300

    build: :tada: iniciando projeto com spring initializer

---
+++ src/main/java/me/ceccoff/sdw24/SantanderDevWeek2024CeccoffApplication.java
@@ -0,0 +1,13 @@
+package me.ceccoff.sdw24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWeek2024CeccoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWeek2024CeccoffApplication.class, args);
+	}
+
+}
