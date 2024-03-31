df79047e33dc2140bd000121b442d4aa7baa37c2
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-03-31 14:24:35 -0300

    feat(Aula 3): :sparkles: Consumo de domain port de IA generativa no usecase de perguntas ao campeao

--- src/main/java/me/ceccoff/sdw24/application/AskChampionUseCase.java
+++ src/main/java/me/ceccoff/sdw24/application/AskChampionUseCase.java
@@ -3,16 +3,23 @@ package me.ceccoff.sdw24.application;
 import me.ceccoff.sdw24.domain.exception.ChampionNotFoundException;
 import me.ceccoff.sdw24.domain.model.Champion;
 import me.ceccoff.sdw24.domain.ports.ChampionsRepository;
+import me.ceccoff.sdw24.domain.ports.GenerativeAiApi;
 
-public record AskChampionUseCase(ChampionsRepository repository) {
+public record AskChampionUseCase(ChampionsRepository repository, GenerativeAiApi genAiApi) {
 
     public String askChampion(Long championId, String question) {
 
         Champion champion = repository.findById(championId)
                 .orElseThrow(() -> new ChampionNotFoundException(championId));
 
-        String championContext = champion.generateContextByQuestion(question);
+        String context = champion.generateContextByQuestion(question);
+        String objective = """
+                Atue como um assistente com a habilidade de se comportar como os Campeões do League of Legends (LOL).
+                Responsa perguntas incorporando a personalidade e estilo de um determinado Campeão.
+                Segue a pergunta, o nome do Campeão e sua respectiva lore (história):
+                
+                """;
 
-        return championContext;
+        return genAiApi.generateContent(objective, context);
     }
 }
