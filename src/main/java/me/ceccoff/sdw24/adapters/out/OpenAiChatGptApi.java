9b4d47fedb09107537f0af0b19fa500d4390bdb0
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-03-31 15:26:16 -0300

    feat(Aula 3): :sparkles: Adicionado tratamento de erros e opcao condicional para criacao do bean apartir de variavel de ambiente

--- src/main/java/me/ceccoff/sdw24/adapters/out/OpenAiChatGptApi.java
+++ src/main/java/me/ceccoff/sdw24/adapters/out/OpenAiChatGptApi.java
@@ -1,8 +1,10 @@
 package me.ceccoff.sdw24.adapters.out;
 
+import feign.FeignException;
 import feign.RequestInterceptor;
 import me.ceccoff.sdw24.domain.ports.GenerativeAiApi;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpHeaders;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@ConditionalOnProperty(name = "generative-ai.provider", havingValue = "OPENAI", matchIfMissing = true)
 @FeignClient(name = "openAiChatGptApi", url = "${openai.base-url}", configuration = OpenAiChatGptApi.Config.class)
 public interface OpenAiChatGptApi extends GenerativeAiApi {
 
@@ -19,22 +22,34 @@ public interface OpenAiChatGptApi extends GenerativeAiApi {
 
     @Override
     default String generateContent(String objective, String context) {
-        List<Message> messages = List.of(
-                new Message("system", objective),
-                new Message("user", objective)
-        );
+        List<Message> messages = List.of(new Message("system", objective), new Message("user", objective));
+
         OpenAiChatCompletionRequest request = new OpenAiChatCompletionRequest("gpt-3.5-turbo", messages);
 
-        OpenAiChatCompletionResponse response = chatCompletion(request);
+        try {
+            OpenAiChatCompletionResponse response = chatCompletion(request);
 
-        return response.choices().getFirst().message().content();
+            return response.choices().getFirst().message().content();
+        } catch (FeignException httpErrors) {
+            httpErrors.printStackTrace();
+            return "Deu ruim! Erro de comunicação com a API do Chatgpt";
+        } catch (Exception unexpectedError) {
+            unexpectedError.printStackTrace();
+            return "Deu mais ruim ainda! O Retorno da API do Chatgpt não contem os dados esperados";
+        }
     }
 
-    record OpenAiChatCompletionRequest(String model, List<Message> messages) {}
-    record Message(String role, String content) {}
+    record OpenAiChatCompletionRequest(String model, List<Message> messages) {
+    }
 
-    record OpenAiChatCompletionResponse(List<Choice> choices) {}
-    record Choice(Message message) {}
+    record Message(String role, String content) {
+    }
+
+    record OpenAiChatCompletionResponse(List<Choice> choices) {
+    }
+
+    record Choice(Message message) {
+    }
 
     class Config {
         @Bean
